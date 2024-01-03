97b16b4c835057210f90157f505b58948b28580e
Dimitri Croes <dimi.croes@gmail.com>
2024-01-03 13:40:15 +0100

    Update 01-run.sh

--- contrailstage/00-packages-files/01-run.sh
+++ contrailstage/00-packages-files/01-run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone -b Disable_ext_calibration --single-branch https://github.com/contrailcirrus/contrail-cameras.git
-cd contrail-cameras
-cp -r "Raspberry_Pi" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Raspberry_Pi_contrail"
+cp -r "./contrail-cameras/Raspberry_Pi" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Raspberry_Pi_contrail"
