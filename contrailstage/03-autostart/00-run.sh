c3cb1e98e4d8aec5c15e36c0b081272621894093
dimitri <dimitri@Debian10.myguest.virtualbox.org>
2024-01-12 14:01:18 +0100

    change 00-run.sh

--- contrailstage/03-autostart/00-run.sh
+++ contrailstage/03-autostart/00-run.sh
@@ -2,9 +2,9 @@
 
 install -m 755 "./files/autostart.sh" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/autostart.sh"
 
-on_chroot << EOF
-    SCRIPT_PATH="/home/${FIRST_USER_NAME}/autostart.sh"
+SCRIPT_PATH="/home/${FIRST_USER_NAME}/autostart.sh"
+# Escape special characters in the script path
+ESCAPED_SCRIPT_PATH=$(echo "$SCRIPT_PATH" | sed 's/[\/&]/\\&/g')
 
-    # Add the line to run the shell script before "exit 0" in rc.local
-    sed -i "/^exit 0/i $SCRIPT_PATH" /etc/rc.local
-EOF
+# Add the line to run the shell script before "exit 0" in rc.local
+sed -i "/^exit 0/i $ESCAPED_SCRIPT_PATH" "${ROOTFS_DIR}/etc/rc.local"
