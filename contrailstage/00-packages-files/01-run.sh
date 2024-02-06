715fba3329ab5ada20694f2e8924023dc4526691
Dimitri Croes <dimi.croes@gmail.com>
2024-02-06 15:04:47 +0100

    update run file.

--- contrailstage/00-packages-files/01-run.sh
+++ contrailstage/00-packages-files/01-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-cp -r "../../../contrail-cameras/Raspberry_Pi" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Raspberry_Pi_contrail"
+cp -r "../../../Raspberry_Pi" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Raspberry_Pi_contrail"
